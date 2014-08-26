0a2ec052b80285f4c352dbd150419e7658f328f6
tommy-carlos williams <tommy@devgeeks.org>
2014-08-26 20:56:28 +1000

    Add conditionals to support iOS 6

--- src/ios/AppDelegate+privacyscreen.m
+++ src/ios/AppDelegate+privacyscreen.m
@@ -11,13 +11,13 @@ UIImageView *imageView;
 
 @implementation AppDelegate (privacyscreen)
 
-// Taken from https://github.com/phonegap-build/PushPlugin/blob/master/src/ios/AppDelegate%2Bnotification.m 
+// Taken from https://github.com/phonegap-build/PushPlugin/blob/master/src/ios/AppDelegate%2Bnotification.m
 // its dangerous to override a method from within a category.
 // Instead we will use method swizzling. we set this up in the load call.
 + (void)load
 {
     Method original, swizzled;
-
+    
     original = class_getInstanceMethod(self, @selector(init));
     swizzled = class_getInstanceMethod(self, @selector(swizzled_init));
     method_exchangeImplementations(original, swizzled);
@@ -25,48 +25,49 @@ UIImageView *imageView;
 
 - (AppDelegate *)swizzled_init
 {
-  [[UIApplication sharedApplication] ignoreSnapshotOnNextApplicationLaunch];
-  // Add any notification observers here...
-
-	// This actually calls the original init method over in AppDelegate. Equivilent to calling super
-	// on an overrided method, this is not recursive, although it appears that way. neat huh?
-	return [self swizzled_init];
+    if ([UIApplication respondsToSelector:@selector(ignoreSnapshotOnNextApplicationLaunch:)]) {
+        [[UIApplication sharedApplication] ignoreSnapshotOnNextApplicationLaunch];
+        // Add any notification observers here...
+    }
+    // This actually calls the original init method over in AppDelegate. Equivilent to calling super
+    // on an overrided method, this is not recursive, although it appears that way. neat huh?
+    return [self swizzled_init];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-  if (imageView == NULL) {
-    [[UIApplication sharedApplication] ignoreSnapshotOnNextApplicationLaunch];
-    self.window.hidden = NO;
-  } else {
-    [imageView removeFromSuperview];
-  }
+    if (imageView == NULL && [[UIApplication sharedApplication] respondsToSelector:@selector(ignoreSnapshotOnNextApplicationLaunch:)]) {
+        [[UIApplication sharedApplication] ignoreSnapshotOnNextApplicationLaunch];
+        self.window.hidden = NO;
+    } else {
+        [imageView removeFromSuperview];
+    }
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-  // for now, assuming 'Default' is the basename of the splashscreen, with a fallback to hiding the window
-  UIImage *splash = [self imageNamedForDevice: @"Default"];
-  if (splash == NULL) {
-    [[UIApplication sharedApplication] ignoreSnapshotOnNextApplicationLaunch];
-    self.window.hidden = YES;
-  } else {
-    imageView = [[UIImageView alloc]initWithFrame:[self.window frame]];
-    [imageView setImage:splash];
-    [UIApplication.sharedApplication.keyWindow.subviews.lastObject addSubview:imageView];
-  }
+    // for now, assuming 'Default' is the basename of the splashscreen, with a fallback to hiding the window
+    UIImage *splash = [self imageNamedForDevice: @"Default"];
+    if (splash == NULL && [[UIApplication sharedApplication] respondsToSelector:@selector(ignoreSnapshotOnNextApplicationLaunch:)]) {
+        [[UIApplication sharedApplication] ignoreSnapshotOnNextApplicationLaunch];
+        self.window.hidden = YES;
+    } else {
+        imageView = [[UIImageView alloc]initWithFrame:[self.window frame]];
+        [imageView setImage:splash];
+        [UIApplication.sharedApplication.keyWindow.subviews.lastObject addSubview:imageView];
+    }
 }
 
 - (UIImage*)imageNamedForDevice:(NSString*)name
 {
-  if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
-    if (([UIScreen mainScreen].bounds.size.height * [UIScreen mainScreen].scale) >= 1136.0f) {
-      name = [name stringByAppendingString:@"-568h@2x"];
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
+        if (([UIScreen mainScreen].bounds.size.height * [UIScreen mainScreen].scale) >= 1136.0f) {
+            name = [name stringByAppendingString:@"-568h@2x"];
+        }
+    } else if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+        name = [name stringByAppendingString:@"-Portrait"];
     }
-  } else if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
-    name = [name stringByAppendingString:@"-Portrait"];
-  }
-  return [UIImage imageNamed: name];
+    return [UIImage imageNamed: name];
 }
 
 @end
