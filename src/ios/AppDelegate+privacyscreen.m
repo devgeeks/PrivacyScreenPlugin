4b6b5e135cd8badce751b9307b9b10cb06f5cc2c
EddyVerbruggen <eddyverbruggen@gmail.com>
2014-10-15 15:21:58 +0200

    https://github.com/devgeeks/PrivacyScreenPlugin/issues/2 support landscape mode

--- src/ios/AppDelegate+privacyscreen.m
+++ src/ios/AppDelegate+privacyscreen.m
@@ -17,7 +17,7 @@ UIImageView *imageView;
 + (void)load
 {
     Method original, swizzled;
-    
+
     original = class_getInstanceMethod(self, @selector(init));
     swizzled = class_getInstanceMethod(self, @selector(swizzled_init));
     method_exchangeImplementations(original, swizzled);
@@ -46,8 +46,7 @@ UIImageView *imageView;
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-    // for now, assuming 'Default' is the basename of the splashscreen, with a fallback to hiding the window
-    UIImage *splash = [self imageNamedForDevice: @"Default"];
+    UIImage *splash = [UIImage imageNamed:[self getImageName:self.viewController.interfaceOrientation delegate:(id<CDVScreenOrientationDelegate>)self.viewController device:[self getCurrentDevice]]];
     if (splash == NULL && [[UIApplication sharedApplication] respondsToSelector:@selector(ignoreSnapshotOnNextApplicationLaunch:)]) {
         [[UIApplication sharedApplication] ignoreSnapshotOnNextApplicationLaunch];
         self.window.hidden = YES;
@@ -68,6 +67,91 @@ UIImageView *imageView;
         name = [name stringByAppendingString:@"-Portrait"];
     }
     return [UIImage imageNamed: name];
+}
+
+
+// Code below borrowed from the CDV splashscreen plugin (https://github.com/apache/cordova-plugin-splashscreen)
+- (CDV_iOSDevice) getCurrentDevice
+{
+  CDV_iOSDevice device;
+  
+  UIScreen* mainScreen = [UIScreen mainScreen];
+  CGFloat mainScreenHeight = mainScreen.bounds.size.height;
+  CGFloat mainScreenWidth = mainScreen.bounds.size.width;
+  
+  int limit = MAX(mainScreenHeight,mainScreenWidth);
+  
+  device.iPad = (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad);
+  device.iPhone = (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone);
+  device.retina = ([mainScreen scale] == 2.0);
+  device.iPhone5 = (device.iPhone && limit == 568.0);
+  // note these below is not a true device detect, for example if you are on an
+  // iPhone 6/6+ but the app is scaled it will prob set iPhone5 as true, but
+  // this is appropriate for detecting the runtime screen environment
+  device.iPhone6 = (device.iPhone && limit == 667.0);
+  device.iPhone6Plus = (device.iPhone && limit == 736.0);
+  
+  return device;
+}
+
+- (NSString*)getImageName:(UIInterfaceOrientation)currentOrientation delegate:(id<CDVScreenOrientationDelegate>)orientationDelegate device:(CDV_iOSDevice)device
+{
+  // Use UILaunchImageFile if specified in plist.  Otherwise, use Default.
+  NSString* imageName = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"UILaunchImageFile"];
+  
+  NSUInteger supportedOrientations = [orientationDelegate supportedInterfaceOrientations];
+  
+  // Checks to see if the developer has locked the orientation to use only one of Portrait or Landscape
+  BOOL supportsLandscape = (supportedOrientations & UIInterfaceOrientationMaskLandscape);
+  BOOL supportsPortrait = (supportedOrientations & UIInterfaceOrientationMaskPortrait || supportedOrientations & UIInterfaceOrientationMaskPortraitUpsideDown);
+  // this means there are no mixed orientations in there
+  BOOL isOrientationLocked = !(supportsPortrait && supportsLandscape);
+  
+  if (imageName) {
+    imageName = [imageName stringByDeletingPathExtension];
+  } else {
+    imageName = @"Default";
+  }
+  
+  if (device.iPhone5) { // does not support landscape
+    imageName = [imageName stringByAppendingString:@"-568h"];
+  } else if (device.iPhone6) { // does not support landscape
+    imageName = [imageName stringByAppendingString:@"-667h"];
+  } else if (device.iPhone6Plus) { // supports landscape
+    if (isOrientationLocked) {
+      imageName = [imageName stringByAppendingString:(supportsLandscape ? @"-Landscape" : @"")];
+    } else {
+      switch (currentOrientation) {
+        case UIInterfaceOrientationLandscapeLeft:
+        case UIInterfaceOrientationLandscapeRight:
+          imageName = [imageName stringByAppendingString:@"-Landscape"];
+          break;
+        default:
+          break;
+      }
+    }
+    imageName = [imageName stringByAppendingString:@"-736h"];
+    
+  } else if (device.iPad) { // supports landscape
+    if (isOrientationLocked) {
+      imageName = [imageName stringByAppendingString:(supportsLandscape ? @"-Landscape" : @"-Portrait")];
+    } else {
+      switch (currentOrientation) {
+        case UIInterfaceOrientationLandscapeLeft:
+        case UIInterfaceOrientationLandscapeRight:
+          imageName = [imageName stringByAppendingString:@"-Landscape"];
+          break;
+          
+        case UIInterfaceOrientationPortrait:
+        case UIInterfaceOrientationPortraitUpsideDown:
+        default:
+          imageName = [imageName stringByAppendingString:@"-Portrait"];
+          break;
+      }
+    }
+  }
+  
+  return imageName;
 }
 
 @end
